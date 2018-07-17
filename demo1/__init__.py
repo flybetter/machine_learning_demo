ded60871baed3837c57a22de0af61867a30629e3
flybetter <flybetter@163.com>
2018-07-17 08:28:16 +0800

    Initial commit

---
+++ demo1/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+@project= machine_learning_demo
+@file= __init__.py
+@author= wubingyu
+@create_time= 2018/7/16 下午10:39
+"""
+import numpy
+
+
+print(numpy.eye(3))
